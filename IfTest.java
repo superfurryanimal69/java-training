d50a564a98becbca1af7ec1b6f6be4fc3aed3c86
superfurryanimal69 <jjt@clara.co.uk>
2014-10-07 14:33:24 +0100

    Create IfTest.java

---
+++ IfTest.java
@@ -0,0 +1,19 @@
+class IfTest {
+
+public static void main (String[] args) {
+int x = 10;
+
+while(x > 3) {
+
+if (x == 3) {
+System.out.println("x must be 3");
+}
+
+x = x - 1;
+
+}
+
+System.out.println(“This runs no matter what”);
+}
+
+}
