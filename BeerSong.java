2433d03abcb6bcb5c57306d30ff48f725f3ccaf0
superfurryanimal69 <jjt@clara.co.uk>
2014-10-07 15:04:04 +0100

    Update BeerSong.java

--- BeerSong.java
+++ BeerSong.java
@@ -7,18 +7,15 @@ String word = "bottles";
 
 while (beerNum > 0) {
 
-
-if (beerNum == 1) {
-word = "bottle"; // singular, as in ONE bottle.
-}
-
-
 System.out.println(beerNum + " " + word + " of beer on the wall");
 System.out.println(beerNum + " " + word + " of beer.");
 System.out.println("Take one down.");
 System.out.println("Pass it around.");
 beerNum = beerNum - 1;
 
+if (beerNum == 1) {
+word = "bottle"; // singular, as in ONE bottle.
+}
 
 if (beerNum > 0) {
 System.out.println(beerNum + " " + word + " of beer on the wall");
