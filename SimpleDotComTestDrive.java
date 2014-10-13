e040637ed5e1fdae86d849c2b7d0fb0dce105fa9
superfurryanimal69 <jjt@clara.co.uk>
2014-10-13 14:59:39 +0100

    Update SimpleDotComTestDrive.java

--- SimpleDotComTestDrive.java
+++ SimpleDotComTestDrive.java
@@ -1,9 +1,20 @@
 public class SimpleDotComTestDrive {
-public static void main (String[] args) {
-SimpleDotCom dot = new SimpleDotCom();
-int[] locations = {2,3,4};
-dot.setLocationCells(locations);
-String userGuess = "2";
-String result = dot.checkYourself(userGuess);
-}
+public static void main(String[] args) {
+int numOfGuesses = 0;
+GameHelper helper = new GameHelper();
+SimpleDotCom theDotCom = new SimpleDotCom();
+int randomNum = (int) (Math.random() * 5);
+int[] locations = {randomNum, randomNum+1, randomNum+2};
+theDotCom.setLocationCells(locations);
+boolean isAlive = true;
+while(isAlive == true) {
+String guess = helper.getUserInput("enter a number");
+String result = theDotCom.checkYourself(guess);
+numOfGuesses++;
+if (result.equals("kill")) {
+isAlive = false;
+System.out.println("You took " + numOfGuesses + " guesses");
+} // close if
+} // close while
+} // close main
 }
