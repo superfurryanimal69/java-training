b9ee47f57990694595101f881a1677dce1a62708
superfurryanimal69 <jjt@clara.co.uk>
2014-10-14 14:08:07 +0100

    Update DotCom.java

--- DotCom.java
+++ DotCom.java
@@ -1,25 +1,42 @@
+import java.util.ArrayList;
+
 public class DotCom {
-int[] locationCells;
-int numOfHits = 0;
-public void setLocationCells(int[] locs) {
-locationCells = locs;
+
+private ArrayList<String> locationCells;
+
+//NB: Don't need numOfHits variable
+
+public void setLocationCells(ArrayList<String> loc) {
+
+locationCells = loc;
+
 }
-public String checkYourself(String stringGuess) {
-int guess = Integer.parseInt(stringGuess);
-String result = "miss";
-for (int cell : locationCells) {
-if (guess == cell) {
+
+public String checkYourself(String userInput) {
+
+String result = "Miss";
+
+int index = locationCells.indexOf(userInput);
+
+if (index >= 0) {
+
+locationCells.remove(index);
+
+if (locationCells.isEmpty()) {
+
+result = "Kill";
+
+}
+else {
+
 result = "hit";
-numOfHits++;
-break;
+
 }
-} // out of the loop
-if (numOfHits ==
-locationCells.length) {
-result = "kill";
-}
-System.out.println(result);
+
+}//Close if
+
 return result;
-} // close method
-} //
+} //Close method
+
+} //Close class
 
