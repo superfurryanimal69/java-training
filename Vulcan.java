85f1a04cc895a6e9231f4b0bf2dc9a5fc7c67868
superfurryanimal69 <jjt@clara.co.uk>
2014-10-17 15:03:40 +0100

    Create Vulcan.java

---
+++ Vulcan.java
@@ -0,0 +1,21 @@
+public class Vulcan implements Alien {
+
+public void attack() {
+
+System.out.println("I'm using the Vulcan death grip on you!");
+
+}
+
+public void communicate() {
+
+System.out.println("Live long and prosper");
+
+}
+
+public void mindMeld() {
+
+System.out.println("My mind to your mind, my thoughts to your thoughts....");
+
+}
+
+}
