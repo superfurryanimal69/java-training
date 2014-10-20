84e6cb7618f22ce19d2b4a34c127781d854153f8
superfurryanimal69 <jjt@clara.co.uk>
2014-10-20 09:09:21 +0100

    Update UseADuck.java

--- UseADuck.java
+++ UseADuck.java
@@ -1,5 +1,33 @@
 public class UseADuck {
+
 public static void main (String[] args) {
-Duck d = new Duck(42);
+
+//Local variables
+int duckSize = 0;
+String userInput = null; 
+boolean isInteger = false;
+
+//Create a GameHelper Object to get a response from the user
+GameHelper getDuckSize = new GameHelper();
+
+while(!isInteger) {
+
+try {
+
+userInput = getDuckSize.getUserInput("Enter the size of the duck you want to create");
+duckSize = Integer.decode(userInput).intValue();
+
+//If an error isn't thrown it means the user has entered something that can be converted in to an integer
+isInteger = true;
+
+} catch (NumberFormatException num) {
+
+System.out.println("Sorry! I can't convert this (" + userInput +") to an integer");
+
+}
+}
+
+//Create a duck
+Duck d = new Duck(duckSize);
 }
 }
